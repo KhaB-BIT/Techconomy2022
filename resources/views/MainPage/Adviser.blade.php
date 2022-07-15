fbafa47cbf8ae2ff1fb18cdcf57cdb869ba473df
KhaB-BIT <buiminhkha24072001@gmail.com>
2022-07-15 16:23:42 +0700

    del scale in carousel

--- resources/views/MainPage/Adviser.blade.php
+++ resources/views/MainPage/Adviser.blade.php
@@ -4,24 +4,22 @@
         <div class="adviser--title">
             <img src="/img/co van chuyen mon.png" alt="">
         </div>
-        <div class="adviser--title__mobile">
-        <img src="/img/covan mobile.png" alt="">
-        </div>
 
         <div class="owl-carousel owl-theme">
 
-            <div class="item">    
+            <div class="item">
                 <div class="main-item">
                     <img class="main-item__img" src="/img/Thầy Thành.jpeg" alt="">
                     <div class="main-item__info border-gradient">
                         <h1 class="main-item__info--name">TS.ĐẶNG NGỌC HOÀNG THÀNH</h1>
                         <p class="main-item__info--detail mt-0"> Giảng viên Trường Công nghệ và Thiết kế, Đại học UEH.
- <br>
-                            Cố vấn lĩnh vực: <strong> Xây dựng và triển khai phần mềm, hệ thống Digital marketing </strong>
-                         </p>
+                            <br>
+                            Cố vấn lĩnh vực: <strong> Xây dựng và triển khai phần mềm, hệ thống Digital marketing
+                            </strong>
+                        </p>
 
                         <img class="item-bottom-left" src="/img/Line 59.png" alt="">
-                    
+
                         <img class="item-top-right" src="/img/Line 61.png" alt="">
                         <img class="item-line-top" src="/img/Line 62.png" alt="">
                         <img class="item-line-bottom" src="/img/Line 62.png" alt="">
@@ -32,18 +30,22 @@
                 </div>
             </div>
 
-            <div class="item">    
+            <div class="item">
                 <div class="main-item">
                     <img class="main-item__img" src="/img/Thầy Huân.jpg" alt="">
                     <div class="main-item__info border-gradient">
                         <h1 class="main-item__info--name">TS. NGUYỄN HỮU HUÂN</h1>
-                        <p class="main-item__info--detail mt-0">Trưởng Bộ môn Thị trường tài chính, Khoa Ngân hàng, Trường Kinh doanh UEH.
- <br>
-                            Tư vấn về tài chính, tài chính số, kinh tế xã hội cho các đài truyền hình VTV, HTV, ANTV, THV <br>
-                            Cố vấn lĩnh vực: <strong>Cuộc thi Fintech, Công nghệ Blockchain của tập đoàn Phongsupthavy, Fintech của K-group, Techfarm holding,...</strong></p>
+                        <p class="main-item__info--detail mt-0">Trưởng Bộ môn Thị trường tài chính, Khoa Ngân hàng,
+                            Trường Kinh doanh UEH.
+                            <br>
+                            Tư vấn về tài chính, tài chính số, kinh tế xã hội cho các đài truyền hình VTV, HTV, ANTV,
+                            THV <br>
+                            Cố vấn lĩnh vực: <strong>Cuộc thi Fintech, Công nghệ Blockchain của tập đoàn
+                                Phongsupthavy,...</strong>
+                        </p>
 
                         <img class="item-bottom-left" src="/img/Line 59.png" alt="">
-                       
+
                         <img class="item-top-right" src="/img/Line 61.png" alt="">
                         <img class="item-line-top" src="/img/Line 62.png" alt="">
                         <img class="item-line-bottom" src="/img/Line 62.png" alt="">
@@ -54,12 +56,13 @@
                 </div>
             </div>
 
-            <div class="item">    
+            <div class="item">
                 <div class="main-item">
                     <img class="main-item__img" src="/img/Thầy Khanh.jpg" alt="">
                     <div class="main-item__info border-gradient">
                         <h1 class="main-item__info--name">TS. NGÔ TẤN VŨ KHANH</h1>
-                        <p class="main-item__info--detail mt-0">Giám đốc chương trình Cao học IDT - Trường Đại học Kinh tế TP.HCM <br>
+                        <p class="main-item__info--detail mt-0">Giám đốc chương trình Cao học IDT - Trường Đại học Kinh
+                            tế TP.HCM <br>
                             Giám đốc Phát triển Kaspersky Lab <br>
                             Cố vấn lĩnh vực: <strong> Thương mại điện tử</strong></p>
 
@@ -73,15 +76,16 @@
                     </div>
                 </div>
             </div>
-            <div class="item">    
+            <div class="item">
                 <div class="main-item">
                     <img class="main-item__img" src="/img/Thầy Định.jpg" alt="">
                     <div class="main-item__info border-gradient">
                         <h1 class="main-item__info--name">ThS. VÕ HÀ QUANG ĐỊNH</h1>
                         <p class="main-item__info--detail mt-0">Trưởng phòng CNTT, Đại học UEH <br>
-                        Giảng viên Khoa CNTTKD <br>
-                            Cố vấn lĩnh vực: <strong>Xây dựng và triển khai phần mềm, hệ thống lĩnh vực Logistics.</strong>
-</p>
+                            Giảng viên Khoa CNTTKD <br>
+                            Cố vấn lĩnh vực: <strong>Xây dựng và triển khai phần mềm, hệ thống lĩnh vực
+                                Logistics.</strong>
+                        </p>
 
                         <img class="item-bottom-left " src="/img/Line 59.png" alt="">
                         <img class="item-top-right" src="/img/Line 61.png" alt="">
@@ -93,15 +97,15 @@
                     </div>
                 </div>
             </div>
-            <div class="item">    
+            <div class="item">
                 <div class="main-item">
                     <img class="main-item__img" src="/img/Thầy Hùng.jpg" alt="">
                     <div class="main-item__info border-gradient">
                         <h1 class="main-item__info--name">ThS. VÕ MINH HÙNG</h1>
                         <p class="main-item__info--detail mt-0">Trưởng phòng CNTT, Đại học UEH <br>
-                        Giảng viên Khoa Giảng viên Khoa Kế toán <br>
+                            Giảng viên Khoa Giảng viên Khoa Kế toán <br>
                             Cố vấn lĩnh vực: <strong>Quản trị nguồn lực (ERP)</strong>
-</p>
+                        </p>
 
                         <img class="item-bottom-left " src="/img/Line 59.png" alt="">
                         <img class="item-top-right" src="/img/Line 61.png" alt="">
@@ -113,14 +117,15 @@
                     </div>
                 </div>
             </div>
-            <div class="item">    
+            <div class="item">
                 <div class="main-item">
-                    <img class="main-item__img" src="/img/Thầy Hùng.jpg" alt="">
+                    <img class="main-item__img" src="/img/anh ueh.jpg" alt="">
                     <div class="main-item__info border-gradient">
                         <h1 class="main-item__info--name">ThS. LÊ DUY KHANG</h1>
-                        <p class="main-item__info--detail mt-0">Giảng viên khoa Quản trị kinh doanh, Trường Đại học Mở TP.HCM. <br>
+                        <p class="main-item__info--detail mt-0">Giảng viên khoa Quản trị kinh doanh, Trường Đại học Mở
+                            TP.HCM. <br>
                             Cố vấn lĩnh vực: <strong>Cuộc thi Digital marketing</strong>
-</p>
+                        </p>
 
                         <img class="item-bottom-left " src="/img/Line 59.png" alt="">
                         <img class="item-top-right" src="/img/Line 61.png" alt="">
