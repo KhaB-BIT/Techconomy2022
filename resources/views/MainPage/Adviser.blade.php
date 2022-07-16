284b39f1b8d05e12872d4c75a79ec6750c5666bb
thduy2003 <duytrieudong@gmail.com>
2022-07-16 07:59:11 +0700

    ok

--- resources/views/MainPage/Adviser.blade.php
+++ resources/views/MainPage/Adviser.blade.php
@@ -6,7 +6,26 @@
         </div>
 
         <div class="owl-carousel owl-theme">
+        <div class="item">
+                <div class="main-item">
+                    <img class="main-item__img" src="/img/Thầy Khanh.jpg" alt="">
+                    <div class="main-item__info border-gradient">
+                        <h1 class="main-item__info--name">TS. NGÔ TẤN VŨ KHANH</h1>
+                        <p class="main-item__info--detail mt-0">Giám đốc chương trình Cao học IDT - Trường Đại học Kinh
+                            tế TP.HCM <br>
+                            Giám đốc Phát triển Kaspersky Lab <br>
+                            Cố vấn lĩnh vực: <strong> Thương mại điện tử</strong></p>
 
+                        <img class="item-bottom-left " src="/img/Line 59.png" alt="">
+                        <img class="item-top-right" src="/img/Line 61.png" alt="">
+                        <img class="item-line-top" src="/img/Line 62.png" alt="">
+                        <img class="item-line-bottom" src="/img/Line 62.png" alt="">
+                        <img class="item-dot item-dot-1" src="/img/Frame 2210.png" alt="">
+                        <img class="item-dot item-dot-2" src="/img/Frame 2210.png" alt="">
+                        <img class="item-dot item-dot-3" src="/img/Frame 2210.png" alt="">
+                    </div>
+                </div>
+            </div>
             <div class="item">
                 <div class="main-item">
                     <img class="main-item__img" src="/img/Thầy Thành.jpeg" alt="">
@@ -58,26 +77,6 @@
 
             <div class="item">
                 <div class="main-item">
-                    <img class="main-item__img" src="/img/Thầy Khanh.jpg" alt="">
-                    <div class="main-item__info border-gradient">
-                        <h1 class="main-item__info--name">TS. NGÔ TẤN VŨ KHANH</h1>
-                        <p class="main-item__info--detail mt-0">Giám đốc chương trình Cao học IDT - Trường Đại học Kinh
-                            tế TP.HCM <br>
-                            Giám đốc Phát triển Kaspersky Lab <br>
-                            Cố vấn lĩnh vực: <strong> Thương mại điện tử</strong></p>
-
-                        <img class="item-bottom-left " src="/img/Line 59.png" alt="">
-                        <img class="item-top-right" src="/img/Line 61.png" alt="">
-                        <img class="item-line-top" src="/img/Line 62.png" alt="">
-                        <img class="item-line-bottom" src="/img/Line 62.png" alt="">
-                        <img class="item-dot item-dot-1" src="/img/Frame 2210.png" alt="">
-                        <img class="item-dot item-dot-2" src="/img/Frame 2210.png" alt="">
-                        <img class="item-dot item-dot-3" src="/img/Frame 2210.png" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="main-item">
                     <img class="main-item__img" src="/img/Thầy Định.jpg" alt="">
                     <div class="main-item__info border-gradient">
                         <h1 class="main-item__info--name">ThS. VÕ HÀ QUANG ĐỊNH</h1>
@@ -122,9 +121,9 @@
                     <img class="main-item__img" src="/img/Thầy Khang.jpg" alt="">
                     <div class="main-item__info border-gradient">
                         <h1 class="main-item__info--name">ThS. LÊ DUY KHANG</h1>
-                        <p class="main-item__info--detail mt-0">Giảng viên khoa Quản trị kinh doanh, Trường Đại học Mở
-                            TP.HCM. <br>
-                            Cố vấn lĩnh vực: <strong>Cuộc thi Digital marketing</strong>
+                        <p class="main-item__info--detail mt-0">Giảng viên Trường Công nghệ và Thiết kế, Đại học UEH
+ <br>
+                            Cố vấn lĩnh vực: <strong>Digital marketing</strong>
                         </p>
 
                         <img class="item-bottom-left " src="/img/Line 59.png" alt="">
